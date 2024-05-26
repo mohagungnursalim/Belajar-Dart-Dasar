2a2691e175cb5fb9b91ef5294a3dd9d96f6286ac
mohagungnursalim <mohagungnursalim@gmail.com>
2024-05-26 11:55:29 +0800

    feat: conversion

---
+++ bin/tipe_data/conversion.dart
@@ -0,0 +1,13 @@
+void main() {
+  var inputString = '3000';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
+
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+
+  var inToDouble = inputInt.toDouble();
+
+  print(inToDouble);
+}
